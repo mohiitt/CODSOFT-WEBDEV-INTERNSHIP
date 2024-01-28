f115b1296f29ce05b2546bac739721498063b7ad
mohiitt <mohitbarade1@gmail.com>
2024-01-29 01:53:10 +0530

    added typescript

---
+++ Landing_page/script1.js
@@ -0,0 +1,6 @@
+
+      var typed = new Typed('#element', {
+        strings: ['Transform Your Body,Transform Your Life', 'Discover a fitness experience like no other.'],
+        typeSpeed: 30,
+      });
+  
